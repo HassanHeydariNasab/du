8186bdc39b7f6e30e65a0c24e81bdbfb2f11d38a
hsn6 <hsn6@tuta.io>
2021-12-23 11:33:20 +0330

    Add fire

---
+++ ATTRIBUTION.md
@@ -0,0 +1,7 @@
+# Attributions
+
+## sprites
+https://opengameart.org/content/animated-top-down-survivor-player
+
+## sounds
+https://freesound.org/people/michorvath/sounds/427598/
--- player/Player.gd
+++ player/Player.gd
@@ -3,13 +3,14 @@ extends Node2D
 const SPEED = 4
 var velocity: Vector2 = Vector2(0.0, 0.0)
 var body_direction: Vector2 = Vector2(0.0, 0.0)
-var body_state: String = 'idle'
 
 onready var Feet = $Feet
 onready var Body = $Body
+onready var HandgunShoot = $HandgunShoot
+
 
 func _ready():
-	pass
+	Body.play('idle')
 
 func _physics_process(delta):
 	velocity = Input.get_vector('l_left_0', 'l_right_0', 'l_up_0', 'l_down_0', 0.1)
@@ -18,15 +19,14 @@ func _physics_process(delta):
 
 func _process(delta):
 	if not(velocity.x == 0 and velocity.y == 0):
-		Feet.set_rotation(atan2(velocity.y, velocity.x))
-		body_state = 'move'
-		Body.play('move')
-	else:
-		body_state = 'idle'
-		Body.play('idle')
+		Feet.set_rotation(velocity.angle())
+		if (Body.get_animation() == 'idle'):
+			Body.play('move')
 	body_direction = Input.get_vector('r_left_0', 'r_right_0', 'r_up_0', 'r_down_0', 0.1)
 	if not(body_direction.x == 0 and body_direction.y == 0):
-		Body.set_rotation(atan2(body_direction.y, body_direction.x))
+		Body.set_rotation(body_direction.angle())
+	
+	# feet animation
 	if (abs(velocity.length()) > 0.99):
 		Feet.play('run')
 	elif (abs(velocity.x) > 0 or abs(velocity.y) > 0):
@@ -34,8 +34,14 @@ func _process(delta):
 	else:
 		Feet.play('idle')
 
-
 func _input(event):
-	pass
 	#print(event.as_text())
+	if event.is_action_pressed('fire_0'):
+		print('fire!')
+		HandgunShoot.play()
+		Body.play('shoot')
 
+
+func _on_Body_animation_finished():
+	if Body.get_animation() == 'shoot':
+		Body.play('idle')
