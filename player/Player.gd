e75577548d4f9f467951a0ea32127ae2064b3b76
hsn6 <hsn6@tuta.io>
2021-12-23 10:47:41 +0330

    Add body

--- player/Player.gd
+++ player/Player.gd
@@ -2,24 +2,37 @@ extends Node2D
 
 const SPEED = 4
 var velocity: Vector2 = Vector2(0.0, 0.0)
+var body_direction: Vector2 = Vector2(0.0, 0.0)
+var body_state: String = 'idle'
+
+onready var Feet = $Feet
+onready var Body = $Body
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	velocity = Input.get_vector('left0', 'right0', 'up0', 'down0', 0.1)
+	velocity = Input.get_vector('l_left_0', 'l_right_0', 'l_up_0', 'l_down_0', 0.1)
 	self.translate(velocity * SPEED)
 	
 
 func _process(delta):
 	if not(velocity.x == 0 and velocity.y == 0):
-		self.set_rotation(atan2(velocity.y, velocity.x))
-	if (abs(velocity.length()) > 0.99):
-		$feet.play('run')
-	elif (abs(velocity.x) > 0 or abs(velocity.y) > 0):
-		$feet.play('walk')
+		Feet.set_rotation(atan2(velocity.y, velocity.x))
+		body_state = 'move'
+		Body.play('move')
 	else:
-		$feet.play('idle')
+		body_state = 'idle'
+		Body.play('idle')
+	body_direction = Input.get_vector('r_left_0', 'r_right_0', 'r_up_0', 'r_down_0', 0.1)
+	if not(body_direction.x == 0 and body_direction.y == 0):
+		Body.set_rotation(atan2(body_direction.y, body_direction.x))
+	if (abs(velocity.length()) > 0.99):
+		Feet.play('run')
+	elif (abs(velocity.x) > 0 or abs(velocity.y) > 0):
+		Feet.play('walk')
+	else:
+		Feet.play('idle')
 
 
 func _input(event):
