6da3153311a86cd9a6f047052300c086cbf43d04
hsn6 <hsn6@tuta.io>
2021-12-23 12:27:24 +0330

    Add reload

--- ATTRIBUTION.md
+++ ATTRIBUTION.md
@@ -5,3 +5,5 @@ https://opengameart.org/content/animated-top-down-survivor-player
 
 ## sounds
 https://freesound.org/people/michorvath/sounds/427598/
+https://freesound.org/people/CosmicEmbers/sounds/387483/
+https://freesound.org/people/duckduckpony/sounds/204020/
--- player/Player.gd
+++ player/Player.gd
@@ -3,10 +3,14 @@ extends Node2D
 const SPEED = 4
 var velocity: Vector2 = Vector2(0.0, 0.0)
 var body_direction: Vector2 = Vector2(0.0, 0.0)
+const BULLETS: int = 16
+var bullets: int = BULLETS
 
 onready var Feet = $Feet
 onready var Body = $Body
 onready var HandgunShoot = $HandgunShoot
+onready var HandgunFailure = $HandgunFailure
+onready var HandgunReload = $HandgunReload
 onready var GunPosition = $Body/GunPosition
 var Bullet = preload('res://player/Bullet.tscn')
 var _Bullet = null
@@ -26,6 +30,8 @@ func _process(_delta):
 		Feet.set_rotation(velocity.angle())
 		if (Body.get_animation() == 'idle'):
 			Body.play('move')
+	elif Body.get_animation() == 'move':
+		Body.play('idle')
 	body_direction = Input.get_vector('r_left_0', 'r_right_0', 'r_up_0', 'r_down_0', 0.1)
 	if not(body_direction.x == 0 and body_direction.y == 0):
 		Body.set_rotation(body_direction.angle())
@@ -41,17 +47,23 @@ func _process(_delta):
 func _input(event):
 	#print(event.as_text())
 	if event.is_action_pressed('fire_0'):
-		print('fire!')
-		HandgunShoot.play()
-		Body.play('shoot')
-		_Bullet = Bullet.instance()
-		_Bullet.set_global_position(GunPosition.get_global_position())
-		_Bullet.set_rotation(Body.get_rotation())
-		Bullets.add_child(_Bullet)
-		
-		
-
+		if bullets == 0:
+			HandgunFailure.play()
+		else:
+			bullets -= 1
+			Body.play('shoot')
+			HandgunShoot.play()
+			_Bullet = Bullet.instance()
+			_Bullet.set_global_position(GunPosition.get_global_position())
+			_Bullet.set_rotation(Body.get_rotation())
+			Bullets.add_child(_Bullet)
+	if event.is_action_pressed('reload_0'):
+		Body.play('reload')
+		HandgunReload.play()
 
 func _on_Body_animation_finished():
 	if Body.get_animation() == 'shoot':
+		Body.play('idle')
+	elif Body.get_animation() == 'reload':
+		bullets = BULLETS
 		Body.play('idle')
