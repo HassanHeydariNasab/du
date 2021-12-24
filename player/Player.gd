52425238ed1cc06a3ec33bf9d58bd108c19e6b94
hsn6 <hsn6@tuta.io>
2021-12-24 12:00:47 +0330

    Add second player

--- player/Player.gd
+++ player/Player.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var id = '0'
+
 const SPEED = 4
 var velocity: Vector2 = Vector2(0.0, 0.0)
 var body_direction: Vector2 = Vector2(0.0, 0.0)
@@ -14,14 +16,14 @@ onready var HandgunReload = $HandgunReload
 onready var GunPosition = $Body/GunPosition
 var Bullet = preload('res://player/Bullet.tscn')
 var _Bullet = null
-onready var Bullets = get_node('../Bullets')
+onready var Bullets = get_node('../../Bullets')
 
 
 func _ready():
 	Body.play('idle')
 
 func _physics_process(_delta):
-	velocity = Input.get_vector('l_left_0', 'l_right_0', 'l_up_0', 'l_down_0', 0.1)
+	velocity = Input.get_vector('l_left_'+id, 'l_right_'+id, 'l_up_'+id, 'l_down_'+id, 0.1)
 	self.translate(velocity * SPEED)
 	
 
@@ -32,7 +34,7 @@ func _process(_delta):
 			Body.play('move')
 	elif Body.get_animation() == 'move':
 		Body.play('idle')
-	body_direction = Input.get_vector('r_left_0', 'r_right_0', 'r_up_0', 'r_down_0', 0.1)
+	body_direction = Input.get_vector('r_left_'+id, 'r_right_'+id, 'r_up_'+id, 'r_down_'+id, 0.1)
 	if not(body_direction.x == 0 and body_direction.y == 0):
 		Body.set_rotation(body_direction.angle())
 	
@@ -46,7 +48,7 @@ func _process(_delta):
 
 func _input(event):
 	#print(event.as_text())
-	if event.is_action_pressed('fire_0'):
+	if event.is_action_pressed('fire_'+id):
 		if bullets == 0:
 			HandgunFailure.play()
 		else:
@@ -57,7 +59,7 @@ func _input(event):
 			_Bullet.set_global_position(GunPosition.get_global_position())
 			_Bullet.set_rotation(Body.get_rotation())
 			Bullets.add_child(_Bullet)
-	if event.is_action_pressed('reload_0'):
+	if event.is_action_pressed('reload_'+id):
 		Body.play('reload')
 		HandgunReload.play()
 
